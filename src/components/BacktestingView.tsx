dc89b2937054cf177d26cf9d517e7fb46166f593
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-10 23:20:18 -0400

    Updated BacktestingView.tsx

--- src/components/BacktestingView.tsx
+++ src/components/BacktestingView.tsx
@@ -774,7 +774,7 @@ const BacktestingView: React.FC = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {/* Configuration Panel */}
         <div className="lg:col-span-1 space-y-6">
           {/* Global Parameters */}
