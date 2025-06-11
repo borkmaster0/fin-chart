fea568c36f8be5d862e79561c4f50ae2b760b2f5
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-11 00:33:11 -0400

    Updated BacktestingView.tsx

--- src/components/BacktestingView.tsx
+++ src/components/BacktestingView.tsx
@@ -1328,6 +1328,7 @@ const BacktestingView: React.FC = () => {
                 </div>
                 
                 <div ref={chartContainerRef} className="w-full h-[400px]" />
+                <div ref={chartContainerTwo} className="w-full h-[400px]" />
               </div>
             </div>
           ) : (
