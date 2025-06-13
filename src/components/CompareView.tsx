15398da637d4685f1cd711778130c473e9618c4a
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-12 23:11:41 -0400

    Updated CompareView.tsx

--- src/components/CompareView.tsx
+++ src/components/CompareView.tsx
@@ -343,6 +343,7 @@ const ChartExpressionApp: React.FC = () => {
             <label key={sym} className="inline-flex items-center space-x-2">
               <input
                 type="checkbox"
+                checked={selectedPlots.includes(sym)}
                 onChange={() => togglePlot(sym)}
               />
               <span>{sym}</span>
