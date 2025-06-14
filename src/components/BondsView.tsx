287ac33c3c958b22dcf3555689bc75f143f444ee
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-14 03:48:47 -0400

    Updated BondsView.tsx

--- src/components/BondsView.tsx
+++ src/components/BondsView.tsx
@@ -201,7 +201,7 @@ export default function BondView() {
                   </div>
                 </div>
               )}
-            </div>
+              </div>
             {activeTab === 'charts' && (
               <div className="text-center text-gray-500 dark:text-gray-400">Charts will be implemented soon.</div>
             )}
