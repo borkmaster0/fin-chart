cc21ffd27b6ff7a362e996697b4bbacecdf02345
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-14 03:18:44 -0400

    Updated BondsView.tsx

--- src/components/BondsView.tsx
+++ src/components/BondsView.tsx
@@ -134,7 +134,7 @@ export default function BondView() {
                         </tr>
                       </thead>
                       <tbody>
-                        {bondOrderBook.data.map((bond, idx) => (
+                        {bondOrderBook.map((bond, idx) => (
                           <tr
                             key={idx}
                             className="border-t border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-800"
@@ -164,7 +164,7 @@ export default function BondView() {
                         </tr>
                       </thead>
                       <tbody>
-                        {billOrderBook.data.map((bill, idx) => (
+                        {billOrderBook.map((bill, idx) => (
                           <tr
                             key={idx}
                             className="border-t border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-800"
