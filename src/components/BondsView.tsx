ca13be7e17d8312d0eba0d66d143afeababce3ff
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-13 23:03:49 -0400

    Updated BondsView.tsx

--- src/components/BondsView.tsx
+++ src/components/BondsView.tsx
@@ -2,3 +2,11 @@ import React, { useRef, useEffect, useState } from 'react';
 import { createChart, ISeriesApi, CandlestickSeries, ColorType, CrosshairMode } from 'lightweight-charts';
 import { fetchBondOrderBook, fetchBillOrderBook } from '../utils/api';
 
+interface CandlestickData {
+  time: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+}
+
