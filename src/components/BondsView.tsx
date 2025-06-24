30ec3c1e91c31c6637a4583dbdad95864aefb8da
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-23 20:48:19 -0400

    Updated BondsView.tsx

--- src/components/BondsView.tsx
+++ src/components/BondsView.tsx
@@ -92,7 +92,7 @@ export default function BondView() {
         vertLines: { color: isDarkMode ? '#374151' : '#e5e7eb' },
         horzLines: { color: isDarkMode ? '#374151' : '#e5e7eb' },
       },
-      crosshair: { mode: CrosshairMode.Normal },
+      
       priceScale: { borderVisible: false },
       timeScale: { borderVisible: false },
     });
