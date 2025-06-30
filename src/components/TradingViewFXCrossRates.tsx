b939c3c3e5c60c470a5497d6573c37cac1e46d99
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-30 01:32:11 -0400

    Updated TradingViewFXCrossRates.tsx

--- src/components/TradingViewFXCrossRates.tsx
+++ src/components/TradingViewFXCrossRates.tsx
@@ -1,7 +1,7 @@
 // TradingViewWidget.jsx
 import React, { useEffect, useRef, memo } from 'react';
 
-function TradingViewWidget() {
+function TradingViewFXCrossRates() {
   const container = useRef();
 
   useEffect(
@@ -65,4 +65,4 @@ function TradingViewWidget() {
   );
 }
 
-export default memo(TradingViewWidget);
+export default memo(TradingViewFXCrossRates);
