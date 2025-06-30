d7d73d08bbb88012b61dabd5180292fca3f44e76
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-30 01:32:35 -0400

    Updated TradingViewETFHeatmap.tsx

--- src/components/TradingViewETFHeatmap.tsx
+++ src/components/TradingViewETFHeatmap.tsx
@@ -1,7 +1,7 @@
 // TradingViewWidget.jsx
 import React, { useEffect, useRef, memo } from 'react';
 
-function TradingViewWidget() {
+function TradingViewETFHeatmap() {
   const container = useRef();
 
   useEffect(
@@ -40,4 +40,4 @@ function TradingViewWidget() {
   );
 }
 
-export default memo(TradingViewWidget);
+export default memo(TradingViewETFHeatmap);
