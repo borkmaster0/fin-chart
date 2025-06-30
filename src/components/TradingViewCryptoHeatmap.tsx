89ede10607eaf4faf170162e9e594b7d0682ce2b
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2025-06-30 01:32:49 -0400

    Updated TradingViewCryptoHeatmap.tsx

--- src/components/TradingViewCryptoHeatmap.tsx
+++ src/components/TradingViewCryptoHeatmap.tsx
@@ -1,7 +1,7 @@
 // TradingViewWidget.jsx
 import React, { useEffect, useRef, memo } from 'react';
 
-function TradingViewWidget() {
+function TradingViewCryptoHeatmap() {
   const container = useRef();
 
   useEffect(
@@ -39,4 +39,4 @@ function TradingViewWidget() {
   );
 }
 
-export default memo(TradingViewWidget);
+export default memo(TradingViewCryptoHeatmap);
